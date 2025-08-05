5cd08fc2c2d9dcd73635342e8c27f4d3a3fb7218
Sandeep Sagar <sandeep.sagar033@gmail.com>
2025-08-05 18:29:04 -0500

    Calculate the investment logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,44 @@
-import { Component, signal } from '@angular/core';
-import { Header } from "./header/header";
-import { UserInput } from "./user-input/user-input";
+import { Component } from '@angular/core';
+import { Header } from './header/header';
+import { UserInput } from './user-input/user-input';
 
 @Component({
   selector: 'app-root',
   imports: [Header, UserInput],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('investment-calculator');
+  //Calculation will be handled in the UserInput component
+  //and results will be emitted to this component
+  onCalculateInvestmentResults(data: {
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number
+  }) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    console.log('Investment Results:', annualData);
+    // Here you can emit the annualData to a service or component to display it
+   // return annualData;
+  }
 }
